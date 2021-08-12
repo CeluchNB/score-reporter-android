136158f9d5f94f250a24d548750905088171397e
Noah Celuch <noah.celuch@gmail.com>
2021-08-11 23:21:24 -0400

    feat(user): added user service calls to retrofit

--- app/src/main/java/com/noah/scorereporter/network/UserService.kt
+++ app/src/main/java/com/noah/scorereporter/network/UserService.kt
@@ -1,13 +1,24 @@
 package com.noah.scorereporter.network
 
 import com.noah.scorereporter.network.model.LoginUser
+import com.noah.scorereporter.network.model.SignUpUser
 import com.noah.scorereporter.network.model.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface UserService {
     @POST("/user/login")
-    fun login(@Body user: LoginUser) : Call<User>
+    fun login(@Body user: LoginUser): Call<User>
 
+    @GET("/user/profile")
+    fun getProfile(@Header("Authorization") jwt: String): Call<User>
+
+    @POST("/user")
+    fun signup(@Body user: SignUpUser): Call<User>
+
+    @POST("/user/logout")
+    fun logout(@Header("Authorization") jwt: String): Call<User>
 }
