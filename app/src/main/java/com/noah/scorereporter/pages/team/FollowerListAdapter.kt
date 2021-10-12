69cdffd0767c76be7ebd86cb86e9e71d28bb91c2
Noah Celuch <noah.celuch@gmail.com>
2021-10-11 23:51:09 -0400

    feat(team): added ui for follower list

---
+++ app/src/main/java/com/noah/scorereporter/pages/team/FollowerListAdapter.kt
@@ -0,0 +1,29 @@
+package com.noah.scorereporter.pages.team
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.noah.scorereporter.data.model.Follower
+import com.noah.scorereporter.databinding.ItemTeamBinding
+
+class FollowerListAdapter(private val list: List<Follower>) : RecyclerView.Adapter<FollowerListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemTeamBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class ViewHolder(val binding: ItemTeamBinding): RecyclerView.ViewHolder(binding.root) {
+        fun onBind(follower: Follower) {
+            binding.follower = follower
+        }
+    }
+}
--- app/src/main/java/com/noah/scorereporter/pages/team/FollowerListFragment.kt
+++ app/src/main/java/com/noah/scorereporter/pages/team/FollowerListFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.OrientationHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.noah.scorereporter.R
 import com.noah.scorereporter.data.model.Follower
+import com.noah.scorereporter.data.model.Role
 
 class FollowerListFragment : Fragment() {
 
@@ -22,12 +27,27 @@ class FollowerListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val followerList = view.findViewById<RecyclerView>(R.id.list_follower)
+        followerList.layoutManager = LinearLayoutManager(requireContext())
+        followerList.addItemDecoration(DividerItemDecoration(requireContext(), OrientationHelper.VERTICAL))
+
         val bundle = arguments
         if (bundle != null) {
             if (bundle.containsKey("FOLLOWERS")) {
-                Log.d("Noah", "got followers")
-                val followers = bundle.getParcelableArrayList<Follower>("FOLLOWERS")
-                Log.d("Noah", followers?.size.toString())
+                // bundle.getParcelableArrayList<Follower>("FOLLOWERS")
+                val followers = listOf(
+                    Follower("Noah Celuch", Role.FAN),
+                    Follower("Amy Celuch", Role.COACH),
+                    Follower("Peyton Celuch", Role.PLAYER),
+                    Follower("Evan Celuch", Role.PLAYER),
+                    Follower("Henry Smith", Role.PLAYER),
+                    Follower("Simon Smith", Role.PLAYER),
+                    Follower("Arthur Smith", Role.PLAYER)
+                )
+                followers?.let {
+                    val followerAdapter = FollowerListAdapter(it)
+                    followerList.adapter = followerAdapter
+                }
             }
         }
     }
